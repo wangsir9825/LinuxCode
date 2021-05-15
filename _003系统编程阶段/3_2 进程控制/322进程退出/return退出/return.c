820d4811036588f1e59dec869a5c18cab87dffa9
wangsir9825 <wangsir9825@163.com>
2021-05-15 14:38:56 +0800

    利用return退出进程

---
+++ _003系统编程阶段/3_2 进程控制/322进程退出/return退出/return.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc,char* argv[])
+{
+    printf("-------------");
+    sleep(3);
+    return 0;
+    printf("我已经退出了\n");
+}
+
